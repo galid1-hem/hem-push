86ec1e3a01436bbc98708bc2eff1a8d197db1c51
전준엽 <galid1@naver.com>
2021-06-17 14:36:32 +0900

    add gitignore

--- hem-push-server/queue/createNotificationQueue.js
+++
@@ -1,5 +0,0 @@
-const Queue = require("bull");
-const redisDomain = "redis://127.0.0.1:6379";
-
-const createNotificationQueue = new Queue("createNotificationQueue", redisDomain);
-module.exports = createNotificationQueue;
